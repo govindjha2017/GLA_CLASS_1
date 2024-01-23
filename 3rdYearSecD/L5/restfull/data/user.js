8b73db38706a877360e21cdd8e522038b7f5a79d
govind <govindkumarjha2017@gmail.com>
2024-01-23 15:20:33 +0530

    23-01-2024

---
+++ 3rdYearSecD/L5/restfull/data/user.js
@@ -0,0 +1,28 @@
+const user = [
+    {
+        id:1,
+        username:'rohit',
+        password:'rohit1234',
+        city: 'Delhi India'
+    },
+    {
+        id:2,
+        username:'rahul',
+        password:'rahu45',
+        city: 'noida India'
+    },
+    {
+        id:3,
+        username:'ajay',
+        password:'ajay123',
+        city: 'Dehradun India'
+    },
+    {
+        id:4,
+        username:'akhil',
+        password:'akhil#652',
+        city: 'Delhi India'
+    },
+];
+
+module.exports = user;
---
+++ 3rdYearSecD/L5/restfull/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const users = require('./data/user');
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.send('working fine');
+})
+
+app.get('/users',(req,res)=>{
+    res.render('users',{users});
+})
+app.get('/user/new',(req,res)=>{
+    res.render('new');
+})
+
+app.post('/users',(req,res)=>{
+    // console.log(req.body);
+    const {username, password , city} = req.body;
+    const user ={
+        id:users.length+1,
+        username,password,city,
+    }
+    users.push(user);
+
+    res.redirect('/users')
+})
+
+app.get('/user/:id',(req,res)=>{
+    const {id} = req.params;
+    const user = users.find((user)=> user.id==id);
+    // console.log(user);
+    res.render('show',{user})
+})
+
+app.listen(4000,()=>{
+   console.log('server up at port',4000);
+})
