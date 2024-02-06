4cbd2bff3a83474214bd97d1edce1063b3834500
govind <govindkumarjha2017@gmail.com>
2024-02-06 11:10:25 +0530

    06-02-2024

---
+++ 3rdYearSecJ/L7/CSR/public/script.js
@@ -0,0 +1,15 @@
+
+
+fetch('http://localhost:5000/todos')
+    .then((res)=>{
+        return res.json();
+    })
+    .then((datas)=>{
+        console.log(datas.todos);
+        const parent = document.querySelector('#todo');
+        for(let data of datas.todos){
+            let p = document.createElement('p');
+            p.innerHTML=data;
+            parent.appendChild(p);
+        }
+    })
